306714aab22f6bbe20a76b29e4b7bc40b97b67ea
Monster2408 <meoto2408@gmail.com>
2021-08-06 23:59:49 +0900

    お遊び

---
+++ public/constellation.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="ja">
+	<head>
+		<title>遊んでええよ</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="./css/constellation.css">
+		<script type="text/javascript" src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+		<script type="text/javascript" src="./js/stats.js"></script>
+	</head>
+	<body>
+		<canvas></canvas>
+		<script src="./js/constellation.js"></script>
+	</body>
+</html>
