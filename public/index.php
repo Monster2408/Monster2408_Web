161a3f3fe7ed55543b5c3dcdd8337245d4c3b717
Monster2408 <meoto2408@gmail.com>
2022-02-19 17:58:09 +0900

    cssの調整

--- public/index.php
+++ public/index.php
@@ -3,23 +3,25 @@ $function = include_once("./function.php");
 
 printCommonHead();
 ?>
-<div class="all_center"><div class="black_box">
-    <h1>MonsterLifeServer</h1>
-    <ul>
-        <li><a href="https://www.mlserver.xyz/" target="_blank">Web Site</a></li>
-        <li><a href="https://youtube.mlserver.xyz/" target="_blank">YouTube</a></li>
-        <li><a href="https://twitter.mlserver.xyz/" target="_blank">Twitter</a></li>
-        <li><a href="https://twitch.mlserver.xyz/" target="_blank">Twitch</a></li>
-    </ul>
 
-    <h1>Monster2408</h1>
-    <ul>
-        <li><a href="https://github.com/Monster2408" target="_blank">GitHub</a></li>
-        <li><a href="https://twitter.com/monster_2408" target="_blank">Twitter</a></li>
-        <!--<li><a href="./repo">GitHub Source</a></li>-->
-        <li><a href="https://www.mlserver.xyz/blog/category/skript-couse/" target="_blank">Skript講座</a></li>
-        
-    </ul>
-</div></div>
+<div class="all_center">
+    <div class="black_box">
+        <h1>MonsterLifeServer</h1>
+        <ul>
+            <li><a href="https://www.mlserver.xyz/" target="_blank">Web Site</a></li>
+            <li><a href="https://youtube.mlserver.xyz/" target="_blank">YouTube</a></li>
+            <li><a href="https://twitter.mlserver.xyz/" target="_blank">Twitter</a></li>
+            <li><a href="https://twitch.mlserver.xyz/" target="_blank">Twitch</a></li>
+        </ul>
+
+        <h1>Monster2408</h1>
+        <ul>
+            <li><a href="https://github.com/Monster2408" target="_blank">GitHub</a></li>
+            <li><a href="https://twitter.com/monster_2408" target="_blank">Twitter</a></li>
+            <!--<li><a href="./repo">GitHub Source</a></li>-->
+            <li><a href="https://www.mlserver.xyz/blog/category/skript-couse/" target="_blank">Skript講座</a></li>
+        </ul>
+    </div>
+</div>
 
 <?php printCommonFoot(); ?>
