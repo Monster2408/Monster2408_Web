d8dca28257c0c8bdb7b9fe589dfe0a84d61da550
Monster2408 <meoto2408@gmail.com>
2022-02-19 17:22:08 +0900

    urlの修正

--- public/index.php
+++ public/index.php
@@ -17,7 +17,7 @@ printCommonHead();
         <li><a href="https://github.com/Monster2408" target="_blank">GitHub</a></li>
         <li><a href="https://twitter.com/monster_2408" target="_blank">Twitter</a></li>
         <!--<li><a href="./repo">GitHub Source</a></li>-->
-        <li><a href="https://www.mlserver.xyz/blog/skript-couse-introduction-custom-command-argument/" target="_blank">Skript講座</a></li>
+        <li><a href="https://www.mlserver.xyz/blog/category/skript-couse/" target="_blank">Skript講座</a></li>
         
     </ul>
 </div></div>
