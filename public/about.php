5eae461ba6cf886e75ceaf88a63f3a99b5a4d9a0
Monster2408 <meoto2408@gmail.com>
2024-05-03 02:33:46 +0900

    up

--- public/about.php
+++ public/about.php
@@ -18,7 +18,7 @@ $func->addStyle('/assets/css/style.min.css');
                 <h3>Monster2408</h3>
                 <p>Minecraftでミニゲーム企画を開催しているMonsterLifeServerの運営で開発やYouTubeに投稿する動画の編集などをしています。</p>
                 <p>元々はコマンドでミニゲーム企画を開催していましたが限界を感じてSkriptを使い始めました。そこから5つほど企画を作り，バグなどの原因でプラグインの勉強を始めました。</p>
-                <p>本格的な開発者歴自体は4年ぐらいです。Skript以外ではHTML,CSS,JavaScript,PHP,Python,MySQL,Cなどを使てシステム設計をしています。</p>
+                <p>本格的な開発者歴自体は5年ぐらいです。Skript以外ではHTML,CSS,JavaScript,PHP,Python,MySQL,Cなどを使てシステム設計をしています。</p>
                 <p>仕事でも似たようなことをしています。</p>
             </div>
         </div>
--- public/projects.php
+++ public/projects.php
@@ -13,7 +13,54 @@ $func->addStyle('/assets/css/style.min.css');
     </head>
     <body>
         <?php include('./assets/include/header.php'); ?>
-        <div class="contents"></div>
+        <div class="contents">
+            <h1>プロジェクト</h1>
+            <p>現在進行中のプロジェクトです。</p>
+            <div class="d-flex card-list justify-content-around flex-wrap flex-row">
+                <a href="https://www.mlserver.xyz/" target="_blank" class="card-a-tag">
+                    <div class="card-box">
+                        <h1>MonsterLifeServer</h1>
+                        <p>MonsterLifeServerはMinecraftでミニゲーム企画を開催しているサーバーです。</p>
+                    </div>
+                </a>
+                <a href="https://youtube.mlserver.xyz/" target="_blank" class="card-a-tag">
+                    <div class="card-box">
+                        <h1>MonsterLifeServerのYouTube</h1>
+                        <p>MonsterLifeServerのYouTubeチャンネルです。</p>
+                    </div>
+                </a>
+                <a href="https://youtube.mlserver.xyz/" target="_blank" class="card-a-tag">
+                    <div class="card-box">
+                        <h1>MonsterLifeServerのYouTube</h1>
+                        <p>MonsterLifeServerのYouTubeチャンネルです。</p>
+                    </div>
+                </a>
+                <a href="https://youtube.mlserver.xyz/" target="_blank" class="card-a-tag">
+                    <div class="card-box">
+                        <h1>MonsterLifeServerのYouTube</h1>
+                        <p>MonsterLifeServerのYouTubeチャンネルです。</p>
+                    </div>
+                </a>
+                <a href="https://youtube.mlserver.xyz/" target="_blank" class="card-a-tag">
+                    <div class="card-box">
+                        <h1>MonsterLifeServerのYouTube</h1>
+                        <p>MonsterLifeServerのYouTubeチャンネルです。</p>
+                    </div>
+                </a>
+                <a href="https://youtube.mlserver.xyz/" target="_blank" class="card-a-tag">
+                    <div class="card-box">
+                        <h1>MonsterLifeServerのYouTube</h1>
+                        <p>MonsterLifeServerのYouTubeチャンネルです。</p>
+                    </div>
+                </a>
+                <a href="https://youtube.mlserver.xyz/" target="_blank" class="card-a-tag">
+                    <div class="card-box">
+                        <h1>MonsterLifeServerのYouTube</h1>
+                        <p>MonsterLifeServerのYouTubeチャンネルです。</p>
+                    </div>
+                </a>
+            </div>
+        </div>
         <?php include('./assets/include/footer.php'); ?>
     </body>
 </html>
