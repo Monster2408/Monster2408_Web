654aa3d65a9d31bd53907315b80d5795dddcf908
Monster2408 <meoto2408@gmail.com>
2024-06-01 01:42:42 +0900

    up

--- public/index.php
+++ public/index.php
@@ -18,13 +18,16 @@ $func->addStyle('/assets/css/style.min.css');
                 <?php include('./assets/include/profile.php'); ?>
             </div>
             <div class="p-1">
-                <div class="title"><h2>アクセス解析中</h2></div>
+                <div class="title"><h2 lang="ja">アクセス解析中</h2><h2 lang="en">Access analysis underway</h2></div>
                 <div class="body">
                     <div class="inner">
                         <div class="system-area count-area">
-                            <p>総アクセス数：<?php echo $viewCountAll; ?></p>
-                            <p>今日のアクセス数：<?php echo $viewCountToday; ?></p>
-                            <p>昨日のアクセス数：<?php echo $viewCountYesterday; ?></p>
+                            <p lang="ja">総アクセス数：<?php echo $viewCountAll; ?></p>
+                            <p lang="en">Total number of accesses: <?php echo $viewCountAll; ?></p>
+                            <p lang="ja">今日のアクセス数：<?php echo $viewCountToday; ?></p>
+                            <p lang="en">Today's access: <?php echo $viewCountToday; ?></p>
+                            <p lang="ja">昨日のアクセス数：<?php echo $viewCountYesterday; ?></p>
+                            <p lang="en">Yesterday's access: <?php echo $viewCountYesterday; ?></p>
                         </div>
                     </div>
                 </div>
